18f4d6e61ab24cefe4bcbe5101ce41e985762af1
eden zewdu <edenzewdu434@gmail.com>
2024-03-04 21:24:28 +0300

    updated

--- src/api.js
+++ src/api.js
@@ -1,11 +1,11 @@
 export const geoApiOptions = {
   method: "GET",
   headers: {
-    "X-RapidAPI-Key": "",// enter your rapid api key here
+    "X-RapidAPI-Key": "231a0a9003mshfed422c6ec1c573p19dc8fjsnd7dd872908cc",
     "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
   },
 };
 export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
 
 export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
-export const WEATHER_API_KEY = ""; // enter your key from openweather API
+export const WEATHER_API_KEY = "0582c26dea732ab97210dc32f5b215b3";
